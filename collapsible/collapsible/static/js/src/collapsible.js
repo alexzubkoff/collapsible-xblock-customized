c3565c3d7b36662721df1405f10810318ffd1b51
Alexander <alexzubkoff123@gmail.com>
2022-12-29 01:26:38 +0200

    Customized collapsiblexblock html, js, css

--- collapsible/collapsible/static/js/src/collapsible.js
+++ collapsible/collapsible/static/js/src/collapsible.js
@@ -7,7 +7,7 @@ function CollapsibleXBlock(runtime, element) {
 
     var handlerUrl = runtime.handlerUrl(element, 'increment_count');
 
-    $('p', element).click(function(eventObject) {
+    $('button', element).click(function(eventObject) {
         $.ajax({
             type: "POST",
             url: handlerUrl,
@@ -18,5 +18,19 @@ function CollapsibleXBlock(runtime, element) {
 
     $(function ($) {
         /* Here's where you'd do things on page load. */
+        var coll = document.getElementsByClassName("collapsible");
+        var i;
+
+        for (i = 0; i < coll.length; i++) {
+          coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+              content.style.display = "none";
+            } else {
+              content.style.display = "block";
+            }
+          });
+        }
     });
 }
